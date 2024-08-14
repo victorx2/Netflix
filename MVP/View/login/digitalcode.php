2522f2a9db409c88d2aeb9fa66df0930fc4afe07
Victor Carrillo <victorcarrillox2@gmail.com>
2024-08-14 18:19:33 -0400

    Nuevos cambios apenas estoy modificando el CSS de manera puritano

--- MVP/View/login/digitalcode.php
+++ MVP/View/login/digitalcode.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
-
     <script src="./assets/js/color-modes.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,186 +8,191 @@
     <meta name="author" content="Víctor Andrés Carrillo Barreto">
     <meta name="generator" content="Victor">
     <title>DIGITAL CODE</title>
-    <link rel="stylesheet" href="./assets/css/bootstrap-utilities.css">
-    <link rel="stylesheet" href="./assets/css/bootstrap.css">
-    <link rel="stylesheet" href="./assets/sass/style.css">
-
-
-
+    <!-- <link rel="stylesheet" href="./assets/css/bootstrap-utilities.css"> -->
+    <!-- <link rel="stylesheet" href="./assets/css/bootstrap.css"> -->
+    <!-- <link rel="stylesheet" href="./assets/css/digitalcode.css"> -->
+    <!-- C:\laragon\www\DigitalCode\assets\sass\style.css -->
 </head>
 
 <style>
-    .row {
-        background-color: yellow;
-    }
+.dg {
+   
+}
+*,
+::after,
+::before {
+  box-sizing: border-box;
+   
+}
 
-    .row-cols-2 {
-        background-color: orange;
-    }
+body {
+
+}
+
+.dg-relative {
+  position: relative;
+  width: 100vw; 
+  height: 100vh;  
+  background-size: cover;
+  background-position: center center;
+}
+
+.digital {
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000000;
+}
+
+
+
+.dg-hidden {
+  position: absolute;
+  left: 25px;
+  top: 35px;
+  width: min(90%, 30px);
+  margin: 30px 0;
+  border: 7.5rem solid white;
+  z-index: 10;
+  background-color: white;
+  background: blue;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+}
+
+.dg-h1 {
+
+}
+
+.dg-container,
+.dg-container-lg .dg-container-fluid {
+  position: relative;
+  height: 6.5vh;
+  width: min(90%, 30px);
+  margin: 0 auto;
+  border: 7rem solid blue;
+  z-index: 10;
+  background-color: blue;
+  background: blue;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+}
+
+#dg-position {
+  position: relative;
+  top: 20px;
+  left: 20px;
+  background: blue;
+}
+
+#dg-position-top {
+ 
+}
+
+.dg-container > .dg-container-sm {
+
+}
+
+.dg-header-container {
+  padding: 0.75rem 1.25rem;
+  margin-bottom: 0;
+  background-color: #f8f9fc;
+  border-bottom: 1px solid #e3e6f0;
+}
+
+.dg-title {
+  background-color: white;
+  text-align: center;
+  font-size: 5rem;
+  font-weight: 400;
+  line-height: 1.5;
+  width: auto;
+  height: auto;
+}
+
+.dg-body {
+  flex: 1 1 auto;
+  min-height: 1px;
+  padding: 1.25rem;
+}
+
+.dg-form-group {
+  background-color: white;
+  text-align: center;
+  font-size: 5rem;
+  font-weight: 400;
+  line-height: 1.5;
+  width: auto;
+  height: auto;
+}
+
+.dg-form-input {
+  background-color: white;
+  text-align: center;
+  font-size: 5rem;
+  font-weight: 400;
+  line-height: 1.5;
+  width: auto;
+  height: auto;
+}
+
+body {
+}
+
 </style>
 
 <body>
-    <abbr title="">xd</abbr>
-    <address>xdd</address>
-    <dt>xdd</dt>
-    <dd>xdd</dd>
-    <blockquote>xdd</blockquote>
-    <b>xdd</b>
-    <strong>xdd</strong>
-    <mark>xdd</mark>
-    <sub>xdd</sub>
-    <sup>xdd</sup>
-    <pre>xdd</pre>
-    <code>xdd</code>
-    <kbd>xdd</kbd>
-    <samp>xdd</samp>
-    <caption>xdd</caption>
-    <legend>xdd</legend>
-    <iframe src="" frameborder="0">xdd</iframe>
-    <summary>xdd</summary>
 
-    <div class="container">
-        <form action="" method="post">
-            <h1 class="text-bg-primary">xdd</h1>
-            <h1 class="text-bg-secondary">xdd</h1>
-            <h1 class="text-bg-success">xdd</h1>
-            <h1 class="text-bg-info">xdd</h1>
-            <h1 class="text-bg-warning">xdd</h1>
-            <div class="row-cols-2">
-                <div class="col">
-                    xdd
-                </div>
-                <div class="col">
-                    xdd
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    xdd
-                </div>
-                <div class="col">
-                    xdd
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    xdd
-                </div>
-                <div class="col">
-                    xdd
-                </div>
-            </div>
-    </div>
-    <div class="container">
-        <form action="" method="post">
-            <div class="row-cols-2">
-                <div class="col">
-                    <input type="text">
-                </div>
-                <div class="col">
-                    <input type="text">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <input type="text">
-                </div>
-                <div class="col">
-                    <input type="text">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <input type="text">
-                </div>
-                <div class="col">
-                    <input type="text">
-                </div>
-            </div>
-    </div>
-    <div class="|"></div>
-    </div>
-    </form>
-    </div>
-
-    <!--     <div class="accordion accordion-flush" id="accordionFlushExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                    Accordion Item #1
-                </button>
-            </h2>
-            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the
-                    <code>.accordion-flush</code> class. This is the first item's accordion body.
-                </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                    Accordion Item #2
-                </button>
-            </h2>
-            <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the
-                    <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this
-                    being filled with some actual content.
-                </div>
-            </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                    Accordion Item #3
-                </button>
-            </h2>
-            <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the
-                    <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting
-                    happening here in terms of content, but just filling up the space to make it look, at least at first
-                    glance, a bit more representative of how this would look in a real-world application.
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <form action="<?php isset($_REQUEST["/assets"]) ?>" method="post">
-        <div class="">
-            <div class="">
-                <div class="row">
-                    <div class="col">
-                        <h2 class="accordion-header">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
-                            <div id="emailHelp" class="form-text"></div>
-                        </h2>
+    <div class="dg-relative"> <!-- Padre -->
+        <div class="digital"> <!-- Hijo -->
+            <div class="dg-hidden"> <!--  -->
+              <div class="dg-container">
+                <div class="dg-header-container">
+                  <div class="dg-title">
+                    <div class="dg-body">
+                      <div class="dg-form-group">
+                      <label class="for">XD</label><input type="text" class="dg-form-input" id="dg-form">
+                      </div>
+                      <div class="dg-form-group">
+                      <label class="for">XD</label>
+                      <input type="text" class="dg-form-input" id="dg-form">
                     </div>
-                    <div class="col">
-                        <h2 class="accordion-header">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password">
-                        </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+ 
+ <!-- <nav></nav> -->
+    <!-- <div class="dg-relative" :digitalcodeurl> relative
+        <div class="digital"> relative
+            <div class="dg-hidden" id="dg-position-top">Caja1</div>
+            <div class="dg-container" id="dg-position">
+                <div class="dg-hidden" id="dg-position-top">
+                    <div class="dg-header-container">
+                        <div class="dg-title">
+                            <div class="dg-body">
+                            <div class="dg-form-group">
+                                <label for="" class="">Email
+                                    <input type="text" class="dg-form-input" id="dg-form">
+                                </label>
+                            </div>
+                            <div class="dg-form-group">
+                                <label for="" class="">Password
+                                    <input type="text" class="dg-form-input" id="dg-form">
+                                </label>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <fieldset class="mb-3">
-            <legend>Radios buttons</legend>
-        </fieldset>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form> -->
-
-    <!-- <form>
-        <div class="mb-3">
-            <label for="" class="form-label">Name</label>
-            <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId" />
-            <small id="helpId" class="text-muted">Help text</small>
-        </div>
-    </form> -->
-
-    <!-- C:\laragon\www\DigitalCode\MVP\View\login\digitalcode.php -->
+    </div> -->
     <script src="../../assets/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
